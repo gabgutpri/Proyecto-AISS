6d3da40de74a6a1b2fbc2676d3c82c4c185d394c
Thiloparn <48439369+Thiloparn@users.noreply.github.com>
2019-04-24 23:44:02 +0200

    Add files via upload

---
+++ aiss-project-template/src/main/java/aiss/controller/Calendar/CalendarCalendarGetController.java
@@ -0,0 +1,48 @@
+package aiss.controller;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import aiss.model.calendar.CalendarList;
+import aiss.model.calendar.Calendars;
+import aiss.model.resource.CalendarResource;
+
+public class CalendarCalendarGetController extends HttpServlet{
+
+    private static final Logger log = Logger.getLogger(CalendarCalendarGetController.class.getName());
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+    	String id = req.getParameter("id");
+        String accessToken = (String) req.getSession().getAttribute("Calendar-token");
+
+        if (accessToken != null && !"".equals(accessToken)) {
+
+            CalendarResource spResource = new CalendarResource(accessToken);
+            Calendars calendar = spResource.getCalendar(id);
+            CalendarList calendarList = spResource.getCalendarList(id);
+            String accessRole = calendarList.getAccessRole();
+
+            if (accessRole == "reader" || accessRole == "owner") {
+                req.setAttribute("calendar", calendar);
+                req.getRequestDispatcher("/CalendarOwnerCalendar.jsp").forward(req, resp);
+            } else {
+                log.warning("This user don't have the role necessary to read this calendar");
+                req.getRequestDispatcher("/?????????????").forward(req, resp);
+            }
+        } else {
+            log.info("Trying to access Google Calendar without an access token, redirecting to OAuth servlet");
+            req.getRequestDispatcher("/AuthController/Calendar").forward(req, resp);
+        }
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        doGet(req, resp);
+    }
+}
