6fada5eeb6656198994ed97d3d879dfc1eb543ce
Abraham281199 <48510669+Abraham281199@users.noreply.github.com>
2019-04-25 16:47:57 +0200

    Add files via upload

--- aiss-project-template/src/main/java/aiss/controller/EventBrite/EventBriteEventListController.java
+++ aiss-project-template/src/main/java/aiss/controller/EventBrite/EventBriteEventListController.java
@@ -1,10 +1,11 @@
 package aiss.controller.EventBrite;
 
-import aiss.model.evenBrite.ListEvent;
+import aiss.model.eventBrite.*;
 import aiss.model.resource.EventBriteResource;
-
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +17,16 @@ public class EventBriteEventListController extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-
+    	String query = req.getParameter("searchQuery");
+		//RequestDispatcher rd = null;
+		//log.log(Level.FINE, "Searching for OMDb movies that contain " + query);
+		
         String accessToken = (String) req.getSession().getAttribute("EventBrite-token");
 
         if (accessToken != null && !"".equals(accessToken)) {
 
             EventBriteResource ebResource = new EventBriteResource(accessToken);
-            ListEvent events = ebResource.getEvents("row");
+            ListEvent events = ebResource.getEvents(query);
 
             if (events != null) {
                 req.setAttribute("events", events);
@@ -32,7 +36,7 @@ public class EventBriteEventListController extends HttpServlet {
                 req.getRequestDispatcher("/AuthController/EventBrite").forward(req, resp);//Cambiar
             }
         } else {
-            log.info("Trying to access Google Drive without an access token, redirecting to OAuth servlet");
+            log.info("Trying to access EventBrite without an access token, redirecting to OAuth servlet");
             req.getRequestDispatcher("/AuthController/EventBrite").forward(req, resp);//Cambiar
         }
     }
