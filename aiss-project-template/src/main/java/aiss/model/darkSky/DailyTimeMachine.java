f87abd8d7765fd282fd2d52d9018b7b12dab0259
LordAdos <lupar.bads@gmail.com>
2019-04-26 11:53:12 +0200

    DarkSky: modelo y controlador más cambio en web.xml con su servlet

---
+++ aiss-project-template/src/main/java/aiss/model/darkSky/DailyTimeMachine.java
@@ -0,0 +1,96 @@
+
+package aiss.model.darkSky;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "latitude",
+    "longitude",
+    "timezone",
+    "daily",
+    "offset"
+})
+public class DailyTimeMachine {
+
+    @JsonProperty("latitude")
+    private Double latitude;
+    @JsonProperty("longitude")
+    private Double longitude;
+    @JsonProperty("timezone")
+    private String timezone;
+    @JsonProperty("daily")
+    private Daily daily;
+    @JsonProperty("offset")
+    private Integer offset;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("latitude")
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    @JsonProperty("latitude")
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    @JsonProperty("longitude")
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    @JsonProperty("longitude")
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    @JsonProperty("timezone")
+    public String getTimezone() {
+        return timezone;
+    }
+
+    @JsonProperty("timezone")
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    @JsonProperty("daily")
+    public Daily getDaily() {
+        return daily;
+    }
+
+    @JsonProperty("daily")
+    public void setDaily(Daily daily) {
+        this.daily = daily;
+    }
+
+    @JsonProperty("offset")
+    public Integer getOffset() {
+        return offset;
+    }
+
+    @JsonProperty("offset")
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ aiss-project-template/src/main/java/aiss/model/resource/DarkSkyResource.java
@@ -0,0 +1,31 @@
+package aiss.model.resource;
+
+import java.util.logging.Logger;
+
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import aiss.model.darkSky.DailyTimeMachine;
+import aiss.model.darkSky.Datum;
+
+
+public class DarkSkyResource {
+	
+	private static final String DARKSKY_API_KEY = "07f61ec3618a79d795e3b3d7657dd414";
+	private static final String URI = "https://api.darksky.net/forecast/";
+	private static final String QUERY = "?exclude=currently,hourly,flags&units=si";
+	private static final Logger log = Logger.getLogger(DarkSkyResource.class.getName());
+	//https://api.darksky.net/forecast/07f61ec3618a79d795e3b3d7657dd414/42.3601,-71.0589,2019-04-25T12:00:00?exclude=currently,hourly,flags&lang=es&units=si
+	
+	public Datum getLatAltDayForecast (String coordenates, String date) {
+		ClientResource cr = null;
+		Datum result = null;
+		try {
+			cr = new ClientResource(URI+DARKSKY_API_KEY+"/"+coordenates+date+"T12:00:00"+QUERY);
+			result = cr.get(DailyTimeMachine.class).getDaily().getData().get(0);
+		}catch (ResourceException re){
+			log.warning("Error when retrieving forecast: " + cr.getResponse().getStatus());
+		}
+		return result;
+	}
+}
