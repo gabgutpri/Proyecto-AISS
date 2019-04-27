2e2fccdbe2992f7cecf076701f4cf832e3536795
LordAdos <lupar.bads@gmail.com>
2019-04-27 20:34:11 +0200

    Libreria JUnit4 añadida y prueba DarkSkyResource

--- aiss-project-template/src/main/java/aiss/model/resource/DarkSkyResource.java
+++ aiss-project-template/src/main/java/aiss/model/resource/DarkSkyResource.java
@@ -15,7 +15,7 @@ public class DarkSkyResource {
 	private static final String URI = "https://api.darksky.net/forecast/";
 	private static final String QUERY = "?exclude=currently,hourly,flags&units=si";
 	private static final Logger log = Logger.getLogger(DarkSkyResource.class.getName());
-	//https://api.darksky.net/forecast/07f61ec3618a79d795e3b3d7657dd414/42.3601,-71.0589,2019-04-25T12:00:00?exclude=currently,hourly,flags&lang=es&units=si
+	// Llamada de ejemplo: https://api.darksky.net/forecast/07f61ec3618a79d795e3b3d7657dd414/42.3601,-71.0589,2019-04-25T12:00:00?exclude=currently,hourly,flags&lang=en&units=si
 	
 	public Datum getLatAltDayForecast (String coordenates, String date) {
 		ClientResource cr = null;
---
+++ aiss-project-template/src/test/java/aiss/model/resource/DarkSkyResourceTest.java
@@ -0,0 +1,19 @@
+package aiss.model.resource;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DarkSkyResourceTest {
+
+	@Test
+	public void testGetLatAltDayForecast() {
+		DarkSkyResource forecast = new DarkSkyResource();
+		assertTrue("Incorrect result", forecast.getLatAltDayForecast("42.3601,-71.0589,", "2019-04-25").getSummary().equals("Partly cloudy overnight.")
+				&& forecast.getLatAltDayForecast("42.3601,-71.0589,", "2019-04-25").getIcon().equals("partly-cloudy-night"));	// Coordenadas de Congress Street, Boston, MA para un día ya pasado.
+		// Solución esperada: Summary= Partly cloudy overnight. Icon= partly-cloudy-night
+		// Prueba para coodenadas mal pasadas
+	}
+
+}
