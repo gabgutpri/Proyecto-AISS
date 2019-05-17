2066199b165d217ed815c2047e9642c0d169aa5f
samuel <samuel@DESKTOP-O949GIS>
2019-05-17 12:23:46 +0200

    Quito los comentarios inutiles

--- aiss-project-template/src/main/java/aiss/controller/EventBrite/EventBriteInfoController.java
+++ aiss-project-template/src/main/java/aiss/controller/EventBrite/EventBriteInfoController.java
@@ -23,28 +23,14 @@ public class EventBriteInfoController extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
     	log.info("entra.");
     	String queryVenue = req.getParameter("venueId");
-    	String queryEvent = req.getParameter("eventId");
     	String date = req.getParameter("date");
         String accessToken = (String) req.getSession().getAttribute("EventBrite-token");
-        //String accessTokenAppEngine = (String) req.getSession().getAttribute("EventBriteAppEngine-token");
-        //El segundo es para cuando se utilice en AppEngine
         if (accessToken != null && !"".equals(accessToken)) {
-
-           //EventBriteResource ebResource = new EventBriteResource(accessToken);
-            //Event event = null;
-            //event=ebResource.getEvent(queryEvent);
-            EventBriteResource ebResource2 = new EventBriteResource(accessToken);
+            
+        	EventBriteResource ebResource2 = new EventBriteResource(accessToken);
             Venue venue=null;
             venue=ebResource2.getDireccion(queryVenue);
-            
-            /*if (event != null) {
-            	log.info("Hola.");
-                req.setAttribute("event", event);
-                req.getRequestDispatcher("/EventInfo.jsp").forward(req, resp);//Cambiar
-            } else {
-                log.info("The files returned are null... probably your token has experied. Redirecting to OAuth servlet.");
-                req.getRequestDispatcher("/AuthController/EventBrite").forward(req, resp);//Cambiar
-            }*/
+
             if (venue != null) {
             	log.info("Adios.");
                 req.setAttribute("venue", venue);
